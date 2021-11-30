3ca04fc3c95db60fac23a98d8644eb66e91e73e1
ahmednasserg <ahmednasser217217@gmail.com>
2021-11-30 13:51:29 +0200

    Update home page
    configured the home page to view catgories dynamically

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const categories = [
+  {
+    name: 'sports',
+    image_src: 'images/sports.png'
+  },
+  {
+    name: 'books',
+    image_src: 'images/books.png'
+  },
+  {
+    name: 'phones',
+    image_src: 'images/phones.png'
+  }
+];
+
 router.get('/', function (req, res) {
-  res.render('home');
+  res.render('home', { categories: categories });
 });
 
 module.exports = router;
