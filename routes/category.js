4ab0738d233c0788fa369493073482441bf1370d
ahmednasserg <ahmednasser217217@gmail.com>
2021-11-30 15:36:37 +0200

    Connect category page to database
    connected category page to database
    to populate products' details

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+const defaultData = require('../database/defaultData');
+const Category = require('../database/categoryModel');
+
+const categories = [
+  {
+    name: 'sports',
+    products: [
+      {
+        name: 'Boxing bag',
+        image_src: 'images/boxing.jpg'
+      },
+      {
+        name: 'Tennis Racket',
+        image_src: 'images/tennis.jpg'
+      }
+    ]
+  }
+];
 
 router.get('/', function (req, res) {
-  res.render('sports');
+  Category.find({ name: req.params.category }, function (err, categories) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.render('category', { category: categories[0] });
+      // res.send(categories[0]);
+    }
+  });
 });
 
 module.exports = router;
