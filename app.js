da10efac5cf6c1f0e7e18008c263f37df89dac65
ahmednasserg <ahmednasser217217@gmail.com>
2021-11-30 12:35:07 +0200

    Add initial routes
    setup home, category, product, and cart initial routes

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// routes
+const homeRoute = require('./routes/home');
+const categoryRoute = require('./routes/category');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRoute);
+app.use('/category/:cat', categoryRoute);
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(3000, function () {
+  console.log('Server is running on port 3000');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
