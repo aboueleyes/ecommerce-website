be32d077523e099674ec9c65f03c4e8d4b1e28b3
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2021-11-30 16:07:37 +0200

    Update default data
    added remaining products

--- database/defaultData.js
+++ database/defaultData.js
@@ -1,38 +1,81 @@
 const Product = require('./productModel');
 const Category = require('./categoryModel');
 
-const product1 = new Product({
+const boxingBag = new Product({
   name: 'Boxing Bag',
   description:
     'Three-Station Heavy Duty Punching Bag Stand by Everlast, 54.00 x 54.00 x 84.00 Inches.',
   imageSrc: 'images/boxing.jpg',
-  videoLink: ''
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
 });
 
-const product2 = new Product({
+const tennisRacket = new Product({
   name: 'Tennis Racket',
   description:
     '2021 New Professional-grade Wilson 1Pc Hend Carbon Fiber Tennis Racket.',
   imageSrc: 'images/tennis.jpg',
-  videoLink: ''
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
 });
 
-const Category1 = new Category({
+const galaxy = new Product({
+  name: 'Galaxy S21 Ultra',
+  description: `The Galaxy S21 has a design similar to 
+  its predecessor, with an Infinity-O display containing a circular cutout in
+   the top center for the front selfie camera. The S21's back panel is 
+   reinforced poly-carbonate similar to the S20 FE and Note 20, while the S21+ 
+   and S21 Ultra use glass.[8] The rear camera array has been integrated into the 
+   phone body and has a metallic surround; the S21 Ultra has 
+   a carbon fiber camera surround for exclusive colors.`,
+  imageSrc: 'images/galaxy.jpg',
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
+});
+
+const leaves = new Product({
+  name: 'Leaves of Grass',
+  description: `A poetry collection by American poet Walt Whitman (1819–1892), 
+  each poem of which is loosely connected and represents the 
+  celebration of his philosophy of life and humanity.`,
+  imageSrc: 'images/leaves.jpg',
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
+});
+
+const iphone = new Product({
+  name: 'iPhone 13 Pro',
+  description: `The iPhone 13 Pro and iPhone 13 Pro Max are smartphones 
+  designed and marketed by Apple Inc. They are the flagship smartphones
+   in the fifteenth generation of the iPhone, succeeding 
+   the iPhone 12 Pro and iPhone 12 Pro Max.`,
+  imageSrc: 'images/iphone.jpg',
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
+});
+
+const sun = new Product({
+  name: 'The Sun and Her Flowers',
+  description: `From Rupi Kaur, the #1 New York Times bestselling 
+  author of milk and honey, comes her long-awaited second collection of
+   poetry. A vibrant and transcendent journey about growth and healing. 
+   Ancestry and honoring one’s roots. Expatriation and rising 
+   up to find a home within yourself.`,
+  imageSrc: 'images/sun.jpg',
+  videoLink: 'https://www.youtube.com/embed/cIkmxCDXBlQ'
+});
+
+const sports = new Category({
   name: 'Sports',
   imageSrc: 'images/sports.png',
-  products: [product1, product2]
+  products: [boxingBag, tennisRacket]
 });
 
-const Category2 = new Category({
+const books = new Category({
   name: 'Books',
   imageSrc: 'images/books.png',
-  products: [product1, product2]
+  products: [leaves, sun]
 });
 
-const Category3 = new Category({
+const phones = new Category({
   name: 'Phones',
   imageSrc: 'images/phones.png',
-  products: [product1, product2]
+  products: [galaxy, iphone]
 });
 
 exports.getDefaultData = function () {
@@ -40,8 +83,12 @@ exports.getDefaultData = function () {
     if (err) {
       console.log(err);
     } else {
-      product1.save();
-      product2.save();
+      boxingBag.save();
+      tennisRacket.save();
+      leaves.save();
+      sun.save();
+      iphone.save();
+      galaxy.save();
     }
   });
 
@@ -49,9 +96,9 @@ exports.getDefaultData = function () {
     if (err) {
       console.log(err);
     } else {
-      Category1.save();
-      Category2.save();
-      Category3.save();
+      sports.save();
+      phones.save();
+      books.save();
     }
   });
 };
