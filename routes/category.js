40e8d01c234c5a5f237796eace1a299012832af5
ahmednasserg <ahmednasser217217@gmail.com>
2021-12-01 16:41:27 +0200

    Update routing
    made route depends on ids insted of names to ne unique

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const Category = require('../database/categoryModel');
 
 router.get('/', function (req, res) {
-  Category.find({ name: req.params.category }, function (err, categories) {
+  Category.find({ _id: req.params.category }, function (err, categories) {
     if (err) {
       res.status(500).send(err);
     } else {
--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,17 @@ const router = express.Router({ mergeParams: true });
 const Product = require('../database/productModel');
 
 router.get('/', function (req, res) {
-  Product.find({ name: req.params.product }, function (err, products) {
+  Product.find({ _id: req.params.product }, function (err, products) {
     if (err) {
       res.status(500).send(err);
     } else {
       res.render('product', { product: products[0] });
     }
   });
+});
+
+router.post('/', function (req, res) {
+  res.send(req.body);
 });
 
 module.exports = router;
