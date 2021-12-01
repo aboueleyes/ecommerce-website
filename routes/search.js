9c9879e79b268017f448d49a77ce560deb217cc3
mohammad.omar <mohammad.omar@robustastudio.com>
2021-12-01 23:55:51 +0200

    add: search functionality & view

---
+++ routes/search.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../database/productModel');
+
+// search route
+router.post('/', (req, res) => {
+    const searchTerm = req.body.searchTerm;
+    
+    if (searchTerm === '') {
+        res.redirect('/');
+    } 
+    else {
+        Product.find({ name: { $regex: `^${searchTerm}`, $options: "i" } }, function (err, products) {
+            if (err) {
+            res.status(500).send(err);
+            } else {
+            res.render('searchresults', { results: products });
+            }
+        });
+    }   
+});
+
+module.exports = router;
