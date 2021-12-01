4eba75f030087f945cc283dbf745f5f7d7841601
ShimaaBetah <shimaabetah1911@gmail.com>
2021-12-01 19:04:50 +0200

    Add addToCart functionality at product route
    update data base according to add product to user cart
    firstly check if it is already added before and increase quantity and if not so add new object to cart with quantity 1

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,23 @@ const router = express.Router({ mergeParams: true });
 const Product = require('../database/productModel');
 const Cart = require('../database/cartModel');
 
+async function addToCart (userName, productId){
+  Cart.findOne({userName : userName}, function(err, cart){
+    let exist = cart.products.some(product => {return product.product === productId})
+    if(exist){
+      cart.products.find ((product, index) => {
+        if (product.product === productId){
+          cart.products[index] = {product : product.product, quantity : product.quantity+1}
+        }
+      })
+    }
+    else {
+      cart.products.push({product : productId , quantity  : 1})
+    }
+    cart.save();
+  })
+}
+
 router.get('/', function (req, res) {
   Product.find({ _id: req.params.product }, function (err, products) {
     if (err) {
@@ -14,9 +31,9 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-  Cart.findOne({ userName: req.session.userName }, function (err, cart) {
-    res.send(cart + req.body.product);
-  });
+  const productId = req.body.product
+  const userName = req.session.userName
+  addToCart(userName, productId)
 });
 
 module.exports = router;
