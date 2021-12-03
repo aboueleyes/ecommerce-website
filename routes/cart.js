39e6589e5c98a950ee982e33a622225178b79ff5
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2021-12-03 14:48:03 +0200

    Update cart route and its view
    updated cart route to view all products from the carts of each registered user.

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Cart = require('../database/cartModel');
+const Product = require('../database/productModel');
 
 router.get('/', function (req, res) {
   if (!req.session.userName) {
     res.redirect('/login');
-  } else {
-    res.render('cart');
+  }
+  else {
+    const userName = req.session.userName;
+    Cart.findOne({ userName: userName }, (err, cart) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      else {
+        const userProducts = cart.products;
+        Promise.all(userProducts.map((userProduct) => {
+          return Product.findOne({ _id: userProduct.product }).exec();
+        })).then((storedProducts) => {
+          console.log(storedProducts);
+          res.render('cart', { userProducts: userProducts, storedProducts: storedProducts });
+        }).catch((err) => {
+          res.status(500).send(err);
+        })
+      }
+    });
   }
 });
 
