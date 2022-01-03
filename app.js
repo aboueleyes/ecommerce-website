8e085f9910261c3fd42f6cbcd93ae9088db6119b
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2022-01-03 17:36:08 +0200

    Refactor app.js
    remove redundant use of login route.

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const homeRoute = require('./routes/home');
 const categoryRoute = require('./routes/category');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
-const loginRouter = require('./routes/login');
+const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
 const searchRoute = require('./routes/search');
 
 const app = express();
-const url ='mongodb+srv://shimaa:Shemo$2864@cluster0.f4td6.mongodb.net/MyDb?retryWrites=true&w=majority';
-async function connect (){
-    await mongoose.connect(url);
+const url = 'mongodb+srv://shimaa:Shemo$2864@cluster0.f4td6.mongodb.net/MyDb?retryWrites=true&w=majority';
+async function connect() {
+  await mongoose.connect(url);
 }
 // connect to database
 //mongoose.connect('mongodb://localhost:27017/ecommerceDB', {
@@ -44,14 +44,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static('public'));
-app.use('/login', loginRouter);
+
+// routes
+app.use('/login', loginRoute);
 app.use('/', homeRoute);
 app.use('/search', searchRoute);
 app.use('/category/:category', categoryRoute);
 app.use('/product/:product', productRoute);
 app.use('/cart', cartRoute);
 app.use('/cart/:product', cartRoute);
-app.use('/login', loginRouter);
 app.use('/register', registerRoute);
 
 
